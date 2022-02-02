f59116678d8017bb1e3036cc04462ca4bc42b29b
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-02 20:55:15 +0300

    Upload photo to a community wall

--- xkcd.py
+++ xkcd.py
@@ -9,6 +9,7 @@ import requests
 
 COMICS_JSON_URL = "https://xkcd.com/{}/info.0.json"
 VK_API_URL = "https://api.vk.com/method"
+COMMUNITY_ID = 210496327
 
 
 def get_comics_json(comics_id: int) -> Optional[dict]:
@@ -42,18 +43,31 @@ def fetch_comics(comics_id: int) -> Optional[str]:
 
 
 def get_upload_server(token: str) -> dict:
-    params = {"access_token": token, "v": "5.124"}
+    params = {"access_token": token, "v": "5.124", "group_id": COMMUNITY_ID}
     response = requests.get(f"{VK_API_URL}/photos.getWallUploadServer", params=params)
     response.raise_for_status()
 
     return response.json()["response"]
 
 
+def push_photo(token: str, filename: str) -> dict:
+    with open(filename, "rb") as file:
+        url = get_upload_server(token)["upload_url"]
+        params = {"access_token": token, "v": "5.124"}
+        files = {
+            "photo": file,
+        }
+        response = requests.post(url, params=params, files=files)
+        response.raise_for_status()
+    return response.json()
+
+
 def main():
     load_dotenv()
     token = os.getenv("VK_TOKEN")
 
-    print(get_upload_server(token))
+    # print(get_upload_server(token))
+    print(push_photo(token, "python.png"))
 
 
 if __name__ == "__main__":
