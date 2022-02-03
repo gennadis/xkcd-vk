c3b091b92176f00d48282f80f197ac18cc952d18
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-03 22:26:35 +0300

    Remove file even if raised error

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def main():
 
     random_id = random.randint(1, xkcd.get_comics_count())
     comics_metadata = xkcd.get_comics_metadata(random_id)
-    filename, title = xkcd.fetch_comics(comics_metadata)
-
-    upload_server_url = vk.get_upload_url(token, group_id)
-    upload_params = vk.upload_photo(token, upload_server_url, filename)
-    photo_id, owner_id = vk.save_photo(token, upload_params, group_id)
-    post_id = vk.publish_wall_post(token, group_id, title, owner_id, photo_id)
-
-    os.remove(filename)
+    try:
+        filename, title = xkcd.fetch_comics(comics_metadata)
+        upload_server_url = vk.get_upload_url(token, group_id)
+        upload_params = vk.upload_photo(token, upload_server_url, filename)
+        photo_id, owner_id = vk.save_photo(token, upload_params, group_id)
+        post_id = vk.publish_wall_post(token, group_id, title, owner_id, photo_id)
+    finally:
+        os.remove(filename)
 
 
 if __name__ == "__main__":
